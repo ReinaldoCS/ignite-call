9dff80264051f9b301fb9a9ea975c6e2593fdd04
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-03-20 12:30:18 -0300

    Calendário e disponibilidade - Tabela de agendamentos

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { useState } from 'react'
 
 import { Calendar } from '@/components/Calendar'
@@ -13,6 +14,12 @@ import {
 export function CalendarStep() {
   const [selectedDate, setSelectDate] = useState<Date | null>(null)
   const isDateSelected = !!selectedDate
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
+
   return (
     <Container isTimePickerOpen={isDateSelected}>
       <Calendar onDateSelect={setSelectDate} selectedDate={selectedDate} />
@@ -20,7 +27,7 @@ export function CalendarStep() {
         <div>
           <TimePicker>
             <TimePickerHeader>
-              terça-feira, <span>20 de setembro</span>
+              {weekDay}, <span>{describedDate}</span>
             </TimePickerHeader>
 
             <TimePickerList>
