20c31de1e377ac94a1bc1b0ded7bc5432df6ff71
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-03-21 17:42:19 -0300

    bugfix: Icon em deploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
-  modularizeImports: {
-    '@phosphor-icons/react': {
-      transform: '@phosphor-icons/react/{{member}}',
-    },
-  },
-  experimental: {
-    optimizePackageImports: ['@phosphor-icons/react'],
-  },
 }
 
 export default nextConfig
