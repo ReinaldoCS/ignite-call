774655d447f30dd325dba201cc7555df1740f0bc
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-03-21 10:09:03 -0300

    Agendamento - Configurando API do Google

---
+++ src/lib/google.ts
@@ -0,0 +1,67 @@
+import dayjs from 'dayjs'
+import { google } from 'googleapis'
+
+import { prisma } from './prisma'
+
+export async function getGoogleOAuthToken(userId: string) {
+  const account = await prisma.account.findFirstOrThrow({
+    where: {
+      provider: 'google',
+      user_id: userId,
+    },
+  })
+
+  const auth = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+  )
+
+  auth.setCredentials({
+    access_token: account.access_token,
+    refresh_token: account.refresh_token,
+    expiry_date: account.expires_at ? account.expires_at * 1000 : null,
+  })
+
+  if (!account.expires_at) {
+    return auth
+  }
+
+  const isAccessTokenExpired = dayjs(account.expires_at * 1000).isBefore(
+    new Date(),
+  )
+
+  if (isAccessTokenExpired) {
+    const { credentials } = await auth.refreshAccessToken()
+
+    const {
+      access_token: accessToken,
+      expiry_date: expiryDate,
+      id_token: idToken,
+      refresh_token: refreshToken,
+      scope,
+      token_type: tokenType,
+    } = credentials
+
+    await prisma.account.update({
+      where: {
+        id: account.id,
+      },
+      data: {
+        access_token: accessToken,
+        expires_at: expiryDate ? Math.floor(expiryDate / 1000) : null,
+        id_token: idToken,
+        refresh_token: refreshToken,
+        scope,
+        token_type: tokenType,
+      },
+    })
+
+    auth.setCredentials({
+      access_token: accessToken,
+      refresh_token: refreshToken,
+      expiry_date: expiryDate,
+    })
+  }
+
+  return auth
+}
